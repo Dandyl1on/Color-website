a50a456cdc077193938c8888ba166366fe81f660
Markus <mflens22@student.aau.dk>
2025-04-20 16:28:26 +0200

    login page

---
+++ connect.php
@@ -0,0 +1,11 @@
+﻿<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "coolers";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+    echo "connection failed";
+}
+?>
---
+++ login.php
@@ -0,0 +1,28 @@
+﻿<?php
+include "connect.php";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+       
+        $email = mysqli_real_escape_string($conn , $_POST['email']);
+        $password = mysqli_real_escape_string($conn , $_POST['password']);
+        
+        $sql = "SELECT * FROM users WHERE email = '$email'";
+        $result = $conn->query($sql);
+        if ($result->num_rows >0){
+            $user = $result->fetch_assoc();
+            
+            if(password_verify($password,$user['password'])){
+                echo "Login succesful!, welcome " . $user['fullname'];
+                
+                header("Location: index.html");
+            }
+            else{
+                echo "Wrong password";
+            }
+        }
+        else{
+            echo "No user with that email";
+        }
+    }
+
+?>
