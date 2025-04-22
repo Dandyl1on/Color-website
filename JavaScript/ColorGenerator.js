d20f543a250db1341d7ce2c35abc4eed33cebf45
Bragh2001 <112693932+Bragh2001@users.noreply.github.com>
2025-04-22 10:00:06 +0200

    HTML CSS Homepage and some generating

---
+++ JavaScript/ColorGenerator.js
@@ -0,0 +1,48 @@
+// Function to generate random color
+function genColorFn() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    let hexColor = `#${randomColor}`;
+    let rgbColor = hexToRgb(hexColor);
+
+    // Update the displayed color values
+    document.getElementById('colorDisplay').style.backgroundColor = hexColor;
+    document.getElementById('colorHex').innerText = hexColor;
+    document.getElementById('colorRgb').innerText = `${rgbColor}`;
+}
+
+function genColorFn2() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    let hexColor = `#${randomColor}`;
+    let rgbColor = hexToRgb(hexColor);
+
+    // Update the displayed color values
+    document.getElementById('colorDisplay').style.backgroundColor = hexColor;
+    document.getElementById('colorHex').innerText = hexColor;
+    document.getElementById('colorRgb').innerText = `${rgbColor}`;
+}
+
+
+// Function to convert HEX to RGB
+function hexToRgb(hex) {
+    let bigint = parseInt(hex.slice(1), 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+    return `${r}, ${g}, ${b}`;
+}
+
+// Function to copy color code
+function cpyFn(type) {
+    let copyText = document.getElementById(type).innerText;
+    navigator.clipboard.writeText(copyText).then(() => {
+        // Display a message for successful copy
+        let copyMessage = document.getElementById('copyMessage');
+        copyMessage.innerText = `${type === 'colorHex' ? 'HEX' : 'RGB'} copied!`;
+        copyMessage.classList.add('animate__fadeIn');
+        setTimeout(() => {
+            copyMessage.classList.remove('animate__fadeIn');
+            copyMessage.innerText = '';
+        }, 2000);
+    });
+}
+
